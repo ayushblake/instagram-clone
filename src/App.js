3e73ac349e1ff415d3ddace82bc1d9b9446c6285
Bhatia <abhati21@LAMU02D7FF9MD6M.uhc.com>
2021-01-09 01:10:30 +0530

    Created Post component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+import Post from './Post';
 
 function App() {
+
+  const[posts,setPosts]=useState([
+    {
+      username:"Ayush",
+      caption:"This is the caption",
+      imageUrl:"https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+    },
+    {
+      username:"Ayush",
+      caption:"This is the caption",
+      imageUrl:"https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="Instagram logo"/>
+      </div>
+      {/* <h1>Hello Alll....</h1>   */}
+      {posts.map(post => (
+        <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+  ))}
     </div>
   );
 }
 
 export default App;
+  
