7b00730d6440f98c85652549219510f9bd21d148
Bhatia <abhati21@LAMU02D7FF9MD6M.uhc.com>
2021-01-12 02:15:21 +0530

    Styled the Image Uploader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React,{useState,useEffect} from 'react'
-import './App.css';
-import Post from './Post';
-import {db}from './firebase'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Post from "./Post";
+import { db, auth } from "./firebase";
 // import classes from '*.module.css';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import ImageUpload from "./ImageUpload";
 // import Button from "@material-ui/core/"
 
 function getModalStyle() {
@@ -20,62 +21,203 @@ function getModalStyle() {
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    position: 'absolute',
+    position: "absolute",
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
 }));
 
-
 function App() {
-
   const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
   const [modalStyle] = React.useState(getModalStyle);
 
-  const[posts,setPosts]=useState([])
-  const[open,setOpen]=useState(false)
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
-
-  useEffect(()=>{
-    db.collection('posts').onSnapshot(snapshot=>{
-      setPosts(snapshot.docs.map(doc => ({
-        id: doc.id,
-        post: doc.data()
-      })))
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user has Logged In...
+        console.log(authUser);
+        setUser(authUser);
+      }
+      // else {
+      //   //user has Logged Out...
+      //   setUser(null);
+      // }
     })
-  },[])
+
+    return () => {
+      //perform some cleanup functions
+      unsubscribe();
+    }
+  }, [user, username]);
+
+  useEffect(() => {
+    db.collection("posts").orderBy('timestamp', 'desc').onSnapshot((snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
+  }, []);
+
+  const signUp = (event) => {
+    event.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((error) => alert(error.message));
+
+    setOpen(false);
+  };
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    auth.signInWithEmailAndPassword(email, password).catch((error) => {
+      alert(error.message);
+    });
+    setOpenSignIn(false);
+  };
 
   return (
     <div className="app">
-        <Modal
-          open={open}
-          onClose={()=>{setOpen(false)}}
-          aria-labelledby="simple-modal-title"
-          aria-describedby="simple-modal-description"
-        >
-       <div style ={modalStyle} className={classes.paper}>
-         <h2>This is inside a Modal</h2>
-       </div>
-     </Modal>
+
+      {/* SIGN UP MODAL */}
+      <Modal
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt="Instagram logo"
+              />
+              <input
+                type="text"
+                value={username}
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <input
+                type="text"
+                value={email}
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                value={password}
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit" onClick={signUp}>
+                Sign Up!
+              </button>
+            </center>
+          </form>
+        </div>
+      </Modal>
+
+      {/* SIGN IN MODAL */}
+      <Modal
+        open={openSignIn}
+        onClose={() => {
+          setOpenSignIn(false);
+        }}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt="Instagram logo"
+              />
+              <input
+                type="text"
+                value={email}
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                value={password}
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit" onClick={signIn}>
+                Sign In!
+              </button>
+            </center>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-          alt="Instagram logo"/>
+          alt="Instagram logo"
+        />
+        {user ? (
+          <button onClick={auth.signOut()}>Logout</button>
+        ) : (
+            <div className="app__loginContainer">
+              <button onClick={() => setOpenSignIn(true)}>Sign IN</button>
+              <button onClick={() => setOpen(true)}>Sign UP</button>
+            </div>
+          )}
       </div>
-      <button onClick={()=> setOpen(true)}>Sign Up!</button>
+
       {/* <h1>Hello Alll....</h1>   */}
-      {posts.map(({id,post}) => (
-        <Post key ={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
-      ))}
-      
+      <div className="app__posts">
+        {
+          posts.map(({ id, post }) => (
+            <Post
+              key={id}
+              username={post.username}
+              caption={post.caption}
+              imageUrl={post.imageUrl}
+            />
+          ))
+        }
+      </div>
+
+
+      {
+        user?.displayName ?
+          (<ImageUpload username={user.displayName} />)
+          : (<h3>Login to Upload please..</h3>)
+      }
     </div>
   );
 }
 
 export default App;
-  
--- src/Post.js
+++ src/Post.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import './Post.css'
-import Avatar from "@material-ui/core/Avatar"
+import React from "react";
+import "./Post.css";
+import Avatar from "@material-ui/core/Avatar";
 
-function Post({username,caption,imageUrl}) {
-    return (
-        <div className="post">
-            <div className="post__header">
-                <Avatar
-                className="post__avatar"
-                alt="Ayush"
-                src="/static/images/avatar/1.jpg"/>
+function Post({ username, caption, imageUrl }) {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt="Ayush"
+          src="/static/images/avatar/1.jpg"
+        />
 
-                <h3>Username</h3>
-            </div>
+        <h3>Username</h3>
+      </div>
 
-            <img className="post__image" src={imageUrl} alt="Post image"/>
+      <img className="post__image" src={imageUrl} alt="" />
 
-             <h4 className="post__text"><strong>{username}</strong>:{caption}</h4>
-        </div>
-    )
+      <h4 className="post__text">
+        <strong>{username}</strong>:{caption}
+      </h4>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
